024122fe52c260ffdc0702da5df21f98a45ea544
adri cardenas jimenez <cardenitas96@gmail.com>
2017-05-24 19:17:03 +0200

    Añadidas las peticiones de amistad su pagina correspondiente

--- AppWebJSF-war/src/java/appWebJSF/PeticionBean.java
+++ AppWebJSF-war/src/java/appWebJSF/PeticionBean.java
@@ -7,6 +7,8 @@ package appWebJSF;
 
 import appWebJSF.ejb.DatosUsuarioFacade;
 import appWebJSF.entity.DatosUsuario;
+import java.math.BigDecimal;
+import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.inject.Named;
 import javax.enterprise.context.RequestScoped;
@@ -22,20 +24,19 @@ public class PeticionBean {
 
     @EJB
     private DatosUsuarioFacade datosUsuarioFacade;
-    
+
     @Inject
     private UsuarioBean usuarioBean;
-    
+
     private DatosUsuario usuarioEnSesion;
-    
-    
+
     /**
      * Creates a new instance of PeticionBean
      */
     public PeticionBean() {
     }
-    
-    public String enviarPeticion(){
+
+    public String enviarPeticion() {
         usuarioEnSesion = this.datosUsuarioFacade.obtenerUsuario(usuarioBean.getEmail(), usuarioBean.getPassword());
         usuarioEnSesion.getPeticionesEnviadas().add(usuarioBean.getUsuario());
         usuarioBean.getUsuario().getPeticionesRecibidas().add(usuarioEnSesion);
@@ -43,5 +44,33 @@ public class PeticionBean {
         this.datosUsuarioFacade.edit(usuarioBean.getUsuario());
         return "index";
     }
-    
+
+    public String rechazarPeticion(DatosUsuario nuevoAmigo) {
+        usuarioEnSesion.getPeticionesRecibidas().remove(nuevoAmigo);
+        nuevoAmigo.getPeticionesEnviadas().remove(usuarioEnSesion);
+        this.datosUsuarioFacade.edit(usuarioEnSesion);
+        this.datosUsuarioFacade.edit(nuevoAmigo);
+        return "peticiones";
+    }
+
+    @PostConstruct
+    public void init() {
+        usuarioEnSesion = this.datosUsuarioFacade.obtenerUsuario(usuarioBean.getEmail(), usuarioBean.getPassword());
+    }
+
+    public String aceptarPeticion(DatosUsuario nuevoAmigo) {
+
+        usuarioEnSesion.getMisAmigos().add(nuevoAmigo);
+        nuevoAmigo.getSoyAmigoDe().add(usuarioEnSesion);
+        usuarioEnSesion.getSoyAmigoDe().add(nuevoAmigo);
+        nuevoAmigo.getMisAmigos().add(usuarioEnSesion);
+
+        usuarioEnSesion.getPeticionesRecibidas().remove(nuevoAmigo);
+        nuevoAmigo.getPeticionesEnviadas().remove(usuarioEnSesion);
+
+        this.datosUsuarioFacade.edit(usuarioEnSesion);
+        this.datosUsuarioFacade.edit(nuevoAmigo);
+        return "peticiones";
+    }
+
 }
